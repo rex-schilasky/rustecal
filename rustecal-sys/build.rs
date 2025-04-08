1c85560f6a271f45753d55510f0dd5d0a7ff7163
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2025-04-08 15:56:35 +0200

    windows / linux build

--- rustecal-sys/build.rs
+++ rustecal-sys/build.rs
@@ -1,25 +1,49 @@
 use std::{env, path::PathBuf};
 
 fn main() {
-    // Get ECAL_HOME from environment
-    let ecal_home = env::var("ECAL_HOME").expect("ECAL_HOME environment variable must be set");
-
-    let include_path = format!("{}/include", ecal_home);
-    let lib_path = format!("{}/lib", ecal_home);
-
-    // Tell cargo to rebuild if wrapper.h changes
-    println!("cargo:rerun-if-changed=wrapper.h");
-
-    // Generate bindings using bindgen
-    let bindings = bindgen::Builder::default()
+    // Prepare bindgen builder
+    let mut builder = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("-I{}", include_path))
         .allowlist_function("eCAL_.*")
         .allowlist_type("eCAL_.*")
         .allowlist_var("eCAL_.*")
         .layout_tests(false)
         .generate_comments(true)
         .derive_default(true)
+        .wrap_unsafe_ops(true);
+
+    if cfg!(target_os = "windows") {
+        // --- Windows: Use ECAL_HOME ---
+        let ecal_home = env::var("ECAL_HOME")
+            .expect("ECAL_HOME environment variable must be set on Windows");
+        let include_path = format!("{}/include", ecal_home);
+        let lib_path = format!("{}/lib", ecal_home);
+
+        println!("cargo:rustc-link-search=native={}", lib_path);
+        println!("cargo:rustc-link-lib=static=ecal_core_c");
+
+        builder = builder.clang_arg(format!("-I{}", include_path));
+
+        // Debug info
+        println!("cargo:warning=Building on Windows");
+        println!("cargo:warning=Using ECAL_HOME = {}", ecal_home);
+    } else if cfg!(target_os = "linux") {
+        // --- Linux: Assume system-wide install ---
+        println!("cargo:rustc-link-lib=dylib=ecal_core_c");
+        println!("cargo:rustc-link-search=native=/usr/local/lib"); // Or /usr/lib if needed
+
+        builder = builder
+            .clang_arg("-I/usr/include")
+            .clang_arg("-I/usr/local/include");
+
+        // Debug info
+        println!("cargo:warning=Building on Linux");
+    } else {
+        panic!("Unsupported platform for rustecal-sys build");
+    }
+
+    // Final bindgen output
+    let bindings = builder
         .generate()
         .expect("Unable to generate bindings");
 
@@ -27,8 +51,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
-    // Link with ecal_core_c.lib
-    println!("cargo:rustc-link-lib=static=ecal_core_c");
-    println!("cargo:rustc-link-search=native={}", lib_path);
 }
